f1d4344bf426e91d8282233059a31ce8a0296655
sayanti dey <sdt2785@gmail.com>
2019-05-19 22:29:05 +0100

    Project setup

---
+++ src/java/test/FindPrimesTest.java
@@ -0,0 +1,20 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * 
+ */
+
+/**
+ * @author sayanti
+ *
+ */
+public class FindPrimesTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
